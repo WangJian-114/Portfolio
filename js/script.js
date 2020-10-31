4a6b3003e8a0af10f6d491e001ead48459ad4b4f
Jian <wangee1998114@gmail.com>
2020-10-30 21:57:43 -0300

    agregando validacion de contactos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,72 @@
+//Codigo JavaScript
+
+(function(){
+    // ejecutar JS en modo estricto
+    "use strict";
+    console.log('Codigo Javascript funciona correctamente');
+
+    document.addEventListener('DOMContentLoaded',function(){
+
+        let errorN = document.getElementById("errorNombre");
+        let errorE = document.getElementById("errorEmail");
+        let nombre = document.getElementById("nombre");
+        let email  = document.getElementById("email");
+        let enviar = document.getElementById("enviar");
+        nombre.addEventListener('blur',validar);
+        email.addEventListener('blur',validarMail);
+        enviar.addEventListener('click',enviarDatos);
+
+        // Funciones
+        function validar(){
+            if(this.value ==''){
+                errorN.style.display="block";
+                errorN.innerHTML="Este campo es obligatorio";
+                this.style.border = '2px solid red';
+                errorN.style.border = '2px solid red';
+            }else {
+                errorN.style.display="none";
+                this.style.border = '1px solid #cccccc';
+            }
+        };
+
+        function validarMail(){
+            //indexOf verifica si existe el valor en el texto, sino existe devuelve -1
+            if(this.value.indexOf("@")>-1){
+                errorE.style.display="none";
+                this.style.border = '1px solid #cccccc';
+            }else{
+                errorE.style.display="block";
+                errorE.innerHTML="Debe tener un @ ";
+                this.style.border = '2px solid red';
+                errorE.style.border = '2px solid red';
+
+            }
+
+        }
+
+        function enviarDatos(event){
+            event.preventDefault();
+            if(nombre.value ==='' || email.value.indexOf("@")>-1){
+                alert('debes completar los campos obligatorio');
+                nombre.focus();
+                email.focus();
+            }else {
+                alert('Gracias por contactarme');
+            }
+        }
+
+    }); //DOM load end
+
+
+})();
+
+
+
+
+
+
+// Codigo jQuery
+
 $(function(){
     'use strict';
     console.log('JQuery funciona correctamente');
@@ -12,20 +81,19 @@ $(function(){
         console.log(enlace);
         $('.mi-tecnologia div').hide();
         $(enlace).show();
-        
-
-        return false;
-
-
-        
+        return false;  
     }
-
-
+    /*
     $('#enviar').on('click',aviso);
     function aviso(){
         alert ('Gracias por contactarme!');
         console.log("esta funcionando")
         
-    }
+    }*/
 
+    // mobile menus
+    $('.enlace img').on('click',function(){
+        $('.submenu').slideToggle();
+    });
+    
 });
